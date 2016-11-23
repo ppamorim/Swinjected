096a45cbcac9dc5c0bc5e295cea658f04c027277
Pedro Paulo Amorim <pepa.amorim@gmail.com>
2016-11-23 22:02:01 +0000

    Fixing files structure

--- swinjected/Domain/Interactor/HomeInteractor.swift
+++
@@ -1,9 +0,0 @@
-protocol HomeInteractor {
-  var callback: HomeCallback? { get set }
-  func requestData()
-}
-
-protocol HomeCallback {
-  func onSuccess(_ items: Array<Item>)
-  func onError(_ error: Swift.Error)
-}
--- swinjected/Domain/Request/Request.swift
+++
@@ -1,4 +0,0 @@
-import Foundation
-
-typealias Success<T> = (_ t: T) -> ()
-typealias Error = (_ error: Swift.Error) -> ()
